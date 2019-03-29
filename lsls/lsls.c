e81b0702f8dc8730b448f2eff9936b749962f169
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-29 10:47:22 -0500

    working on printing sub directories

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
 void listfiles(const char *path)
 {
   struct dirent *ent;
+  struct stat buf;
 
   DIR *dir = opendir(path);
 
@@ -16,7 +18,14 @@ void listfiles(const char *path)
 
   // While files are avaiable lets keep reading
   while ( (ent = readdir(dir)) != NULL ) {
+    stat(ent->d_name, &buf);
+    printf("%10ld ", buf.st_size );
     printf("%s\n", ent->d_name);
+      if (buf.st_size == 512) {
+        while ( (ent = readdir(opendir((ent->d_name))) ) != NULL ) {
+          printf("%s\n", ent->d_name);
+        }
+      }
   };
 
   // Once we're done, lets close directory
@@ -36,7 +45,10 @@ int main()
   printf("Enter path to list files: ");
 
   // User input
-  scanf("%s", path);
+  scanf("%[^\n]", path);
+    if (path[0] == 0) {
+      path[0] = '.';
+    };
 
   // function that will process the input and return files
   listfiles(path);
